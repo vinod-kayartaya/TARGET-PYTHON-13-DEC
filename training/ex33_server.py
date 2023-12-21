f4c703db3a6e060629df7291e635947d7a29455c
Vinod Kumar Kayartaya <vinod@vinod.co>
2023-12-21 13:12:02 +0530

    End of session

--- training/ex33_server.py
+++ training/ex33_server.py
@@ -1,4 +1,6 @@
+import time
 from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
 
 
 def main():
@@ -10,7 +12,17 @@ def main():
     while True:
         # wait for clients to connect
         client_socket, client_addr = server_socket.accept()
-        print(f'Got a connection from {client_addr}')
+        Thread(target=handle_client,
+               args=(client_socket, client_addr)).start()
+
+
+def handle_client(client_socket, client_addr):
+    print(f'Got a connection from {client_addr}')
+    username = client_socket.recv(1024).decode('ascii')
+    msg = f'hello {username} and welcome to python networking'
+    time.sleep(10)
+    client_socket.send(msg.encode('ascii'))
+    print('client socket is being closed.')
 
 
 if __name__ == '__main__':
--- training/ex34_client.py
+++ training/ex34_client.py
@@ -7,6 +7,10 @@ def main():
     addr = ('192.168.1.23', 1234)
     client_socket.connect(addr)
     print(f'connected to {addr} successfully')
+    username = input('Enter your name: ')
+    client_socket.send(username.encode('ascii'))
+    msg = client_socket.recv(1024).decode('ascii')
+    print(f'server says - {msg}')
     print('end of client app')
 
 
